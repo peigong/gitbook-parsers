6c7c4e3fdde1ac0767de263a4ffc5aa0d6d67881
Samy Pessé <samypesse@gmail.com>
2015-01-23 15:24:24 +0100

    Create client side bundle of parsers

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function (grunt) {
+    var path = require("path");
+
+    // Load NPM tasks
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Init GRUNT configuraton
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            dist: {
+                files: {
+                    'gitbook-parsers.js': [
+                        './lib/index.js'
+                    ],
+                },
+                options: {
+                    postBundleCB: function (err, src, next) {
+                        if (err) return next(err);
+                        return next(null, '(function () { var define = undefined; '+src+'; })();')
+                    },
+                    browserifyOptions: {
+                        'standalone': "gitbookParsers"
+                    }
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'gitbook-parsers.min.js': ['gitbook-parsers.js']
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("bower-install", [ "bower-install-simple" ]);
+
+    // Bundle the library
+    grunt.registerTask('bundle', [
+        'browserify',
+        'uglify'
+    ]);
+
+    grunt.registerTask('default', [
+        'bundle'
+    ]);
+};
